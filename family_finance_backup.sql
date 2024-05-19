f9f842059c062964a5eaf8adccc27ccd1a3dd4a2
Orgamliel7 <57177424+Orgamliel7@users.noreply.github.com>
2024-05-19 16:45:44 +0300

    Add files via upload
    My SQL data - example

---
+++ family_finance_backup.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 8.0.37, for Win64 (x86_64)
+--
+-- Host: localhost    Database: family_finance
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `expenses`
+--
+
+DROP TABLE IF EXISTS `expenses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `expenses` (
+  `expense_id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`expense_id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `expenses_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `family_members` (`member_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `expenses`
+--
+
+LOCK TABLES `expenses` WRITE;
+/*!40000 ALTER TABLE `expenses` DISABLE KEYS */;
+INSERT INTO `expenses` VALUES (5,1,500.00,'Groceries','2024-05-19'),(6,2,300.00,'Utilities','2024-05-20'),(7,3,50.00,'Books','2024-05-21');
+/*!40000 ALTER TABLE `expenses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `family_members`
+--
+
+DROP TABLE IF EXISTS `family_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `family_members` (
+  `member_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `relationship` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`member_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `family_members`
+--
+
+LOCK TABLES `family_members` WRITE;
+/*!40000 ALTER TABLE `family_members` DISABLE KEYS */;
+INSERT INTO `family_members` VALUES (1,'Or','Father'),(2,'Sharon','Mother'),(3,'Maayan','Daughter');
+/*!40000 ALTER TABLE `family_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `incomes`
+--
+
+DROP TABLE IF EXISTS `incomes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `incomes` (
+  `income_id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `source` varchar(100) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`income_id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `incomes_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `family_members` (`member_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `incomes`
+--
+
+LOCK TABLES `incomes` WRITE;
+/*!40000 ALTER TABLE `incomes` DISABLE KEYS */;
+INSERT INTO `incomes` VALUES (5,1,10000.00,'Salary','2024-05-19'),(6,2,10000.00,'Salary','2024-05-19'),(7,3,0.00,NULL,NULL);
+/*!40000 ALTER TABLE `incomes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-19 13:15:03
